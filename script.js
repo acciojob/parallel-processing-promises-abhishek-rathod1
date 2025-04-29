12b5de6969256ad3562c52a94996e34c74be16f8
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-29 21:25:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,41 @@
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
+output.innerHTML = "<div id='loading'>Loading Spinner..</div>"
+
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+// let imagesHtml = "";
+
+function downloadImage(url) {
+	return new Promise((resolve, reject)=>{
+		const img = new Image();
+		img.src = url;
+
+		img.onload = () => resolve(img);
+		img.onerror = () => reject(`Failed to download image: ${url}`);
+		
+	})	
+}
+
+
+function downloadImages() {
+	Promise.all(images.map(img=>downloadImage(img.url))).then((imgElements)=>{
+		output.innerHTML = '';
+		imgElements.forEach(img => output.appendChild(img));
+		
+	}).catch((e)=>{
+		output.innerHTML = `<p>Error : ${e}</p>`
+		// console.log(e)
+	})
+	
+}
+
+
+window.addEventListener("DOMContentLoaded", downloadImages);
+
+// ffffddffdfd1f
